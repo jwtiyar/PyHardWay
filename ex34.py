8c9d0dfb494d7de8ecdce0348fb87df5be13a5b8
Jwtiyar Nariman <jwtiyar@gmail.com>
2020-09-07 21:09:33 +0000

    More

---
+++ ex34.py
@@ -0,0 +1,10 @@
+animals = ['bear','tiger','penguin','zebra']
+bearf = animals[0] #Bem Sheweye detwany bangy yekem dane bkey le list.
+print(bearf)
+print(animals[2]) # Yan detwany bem SHewyesh bangy her kameyek bkey le listeke.
+print(animals[3])
+
+Ajellekan = ['mar', 'plng', 'esp']
+yekemdane = Ajellekan[0]
+print("Yekem dane brytye le {}".format(yekemdane))
+print(Ajellekan[1])
