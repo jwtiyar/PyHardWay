2f7f38ca00f67bee0c9d4442f2df21278fd2f18c
jwtiyar <jwtiyar@gmail.com>
2020-10-05 10:05:28 +0300

    Training OOP

--- class-AND-object-Training.py
+++ class-AND-object-Training.py
@@ -27,8 +27,11 @@ person1 = Person("hama", "zorqse", False)
 person2 = Person("Gazi", "kemqse", True)
 
 person1.zanyary()
-person2.zanyary()
+person2.zanyary() #Bem sheweye detwany encamy bdeyt be ewey instance y () y bdeyte chwnke xoy dyarye ke bes instance y person2 werdegret
+#bellam bo ewey xwarewe:
 
+print(Person.zanyary(person1)) #Lere debet instance y bdeyne chwnke class man bangkrdwa, class ysh chend method eky tedaye ke debet pey blleyt
+#kame print bkat bo nmwne wtwmane person1 bere be instance.
 
 r1 = Robot("TOM", "Red", 40)
 r2 = Robot("Jerry", "RED", 30)
